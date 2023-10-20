94a852adab9a4711feb1ba45404bffc88db4cea2
Shubham Lal <lal.shubham027@gmail.com>
2023-10-20 23:23:37 +0530

    send mail with nodemailer transport

--- api/helpers/mailer.js
+++ api/helpers/mailer.js
@@ -1,77 +1,121 @@
 const nodemailer = require("nodemailer");
-
-const { google } = require("googleapis");
 const accountVerificationHtml = require("./accountVerificationHtml");
 const resetPasswordHtml = require("./resetPasswordHtml");
 
-const { OAuth2 } = google.auth;
-const oauth_link = "https://developers.google.com/oauthplayground"
-const { EMAIL, MAILING_ID, MAILING_REFRESH, MAILING_SECRET } = process.env;
-
-const auth = new OAuth2(MAILING_ID, MAILING_SECRET, MAILING_REFRESH, oauth_link);
-
-// SEND VERIFICATION EMAIL
-exports.sendVerificationEmail = (email, name, url) => {
-    auth.setCredentials({
-        refresh_token: MAILING_REFRESH,
-    });
-
-    const accessToken = auth.getAccessToken();
-    const stmp = nodemailer.createTransport({
-        service: "gmail",
+exports.sendVerificationEmail = async (email, name, url) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        service: process.env.SMTP_SERVICE,
+        secure: true,
         auth: {
-            type: "OAuth2",
-            user: EMAIL,
-            clientId: MAILING_ID,
-            clientSecret: MAILING_SECRET,
-            refreshToken: MAILING_REFRESH,
-            accessToken: accessToken,
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD
+        }
     });
+
     const mailOptions = {
-        from: EMAIL,
+        from: process.env.SMTP_MAIL,
         to: email,
         subject: "Clang Social | Account Verification",
         html: accountVerificationHtml(name, url)
-    };
-    stmp.sendMail(mailOptions, (err, res) => {
-        if (err) return err;
-        return res;
-    })
+    }
+
+    await transporter.sendMail(mailOptions);
 };
 
-// SEND RESET CODE
-exports.sendResetCode = (email, name, code) => {
-    auth.setCredentials({
-        refresh_token: MAILING_REFRESH,
+exports.sendResetCode = async (email, name, code) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        service: process.env.SMTP_SERVICE,
+        secure: true,
+        auth: {
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD
+        }
     });
 
-    const accessToken = auth.getAccessToken();
-    const stmp = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            type: "OAuth2",
-            user: EMAIL,
-            clientId: MAILING_ID,
-            clientSecret: MAILING_SECRET,
-            refreshToken: MAILING_REFRESH,
-            accessToken: accessToken,
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
-    });
     const mailOptions = {
-        from: EMAIL,
+        from: process.env.SMTP_MAIL,
         to: email,
         subject: "Clang Social | Reset User Password",
         html: resetPasswordHtml(name, code)
-    };
-    stmp.sendMail(mailOptions, (err, res) => {
-        if (err) return err;
-        return res;
-    })
+    }
+
+    await transporter.sendMail(mailOptions);
 };
+
+// const { google } = require("googleapis");
+
+// const { OAuth2 } = google.auth;
+// const oauth_link = "https://developers.google.com/oauthplayground"
+// const { EMAIL, MAILING_ID, MAILING_REFRESH, MAILING_SECRET } = process.env;
+
+// const auth = new OAuth2(MAILING_ID, MAILING_SECRET, MAILING_REFRESH, oauth_link);
+
+// // SEND VERIFICATION EMAIL
+// exports.sendVerificationEmail = (email, name, url) => {
+//     auth.setCredentials({
+//         refresh_token: MAILING_REFRESH,
+//     });
+
+//     const accessToken = auth.getAccessToken();
+//     const stmp = nodemailer.createTransport({
+//         service: "gmail",
+//         auth: {
+//             type: "OAuth2",
+//             user: EMAIL,
+//             clientId: MAILING_ID,
+//             clientSecret: MAILING_SECRET,
+//             refreshToken: MAILING_REFRESH,
+//             accessToken: accessToken,
+//         },
+//         tls: {
+//             rejectUnauthorized: false,
+//         },
+//     });
+//     const mailOptions = {
+//         from: EMAIL,
+//         to: email,
+//         subject: "Clang Social | Account Verification",
+//         html: accountVerificationHtml(name, url)
+//     };
+//     stmp.sendMail(mailOptions, (err, res) => {
+//         if (err) return err;
+//         return res;
+//     })
+// };
+
+// // SEND RESET CODE
+// exports.sendResetCode = (email, name, code) => {
+//     auth.setCredentials({
+//         refresh_token: MAILING_REFRESH,
+//     });
+
+//     const accessToken = auth.getAccessToken();
+//     const stmp = nodemailer.createTransport({
+//         service: "gmail",
+//         auth: {
+//             type: "OAuth2",
+//             user: EMAIL,
+//             clientId: MAILING_ID,
+//             clientSecret: MAILING_SECRET,
+//             refreshToken: MAILING_REFRESH,
+//             accessToken: accessToken,
+//         },
+//         tls: {
+//             rejectUnauthorized: false,
+//         },
+//     });
+//     const mailOptions = {
+//         from: EMAIL,
+//         to: email,
+//         subject: "Clang Social | Reset User Password",
+//         html: resetPasswordHtml(name, code)
+//     };
+//     stmp.sendMail(mailOptions, (err, res) => {
+//         if (err) return err;
+//         return res;
+//     })
+// };
