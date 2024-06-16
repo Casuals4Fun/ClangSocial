d2b9393294afd9db7847e99d13486d4b54b80739
Shubham Lal <im.shubhamlal@gmail.com>
2024-06-16 12:20:27 +0530

    Delete client directory

--- client/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import CreatePostPopup from "./createPostPopup";
-import RoomInput from "./createPost/RoomInput";
-import FullScreen from "./post/FullScreen";
-
-export {
-    CreatePostPopup,
-    RoomInput,
-    FullScreen
-};
